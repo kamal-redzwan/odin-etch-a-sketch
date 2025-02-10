3bc6d19c556d2cffab8a04b4a043d2cacb45b629
kamal-redzwan <109417261+kamal-redzwan@users.noreply.github.com>
2025-02-10 21:08:29 +0800

    add controls and drawing mode logic

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const drawMode = document.querySelector('#drawMode').value;
+
+// to track when user is in drawing mode
+let isDrawing = false;
+
 function createGrid() {
   const gridContainer = document.querySelector('.grid-container');
-  const gridSize = 16;
+  const gridSize = parseInt(document.querySelector('#grid-size').value);
+
+  //clear any existing grid
+  gridContainer.innerHTML = '';
 
   const containerSize = 640;
   const pixelSize = containerSize / gridSize;
@@ -13,13 +21,63 @@ function createGrid() {
 
     // add pixel class
     pixel.className = 'pixel';
+
     // add pixel sizing
     pixel.style.width = `${pixelSize}px`;
     pixel.style.height = `${pixelSize}px`;
 
-    // append to the container
+    // add event listener for the drawing modes
+    pixel.addEventListener('mousedown', handleMouseDown); //Start drawing when hover
+    pixel.addEventListener('mouseover', handleMouseOver); //Continue drawing
+    pixel.addEventListener('click', handleMouseClick); //Toggle drawing when clicked
+
+    // append pixels to the container
     gridContainer.appendChild(pixel);
   }
 }
 
-createGrid();
+// handle initial mouse press
+function handleMouseDown(e) {
+  // only activate when in hover draw mode
+  if (drawMode === 'hover') {
+    isDrawing = true;
+    draw(e); // start drawing
+  }
+}
+
+// handle mouse movement over pixels
+function handleMouseOver(e) {
+  // only draw if in hover mode
+  if (drawMode === 'hover') {
+    draw(e);
+  }
+}
+
+// handle click event for toggle click mode
+function handleMouseClick(e) {
+  if (drawMode === 'click') {
+    draw(e);
+  }
+}
+
+function draw(e) {
+  if (!isDrawing) return; // only draw when mouse is pressed
+  const color = 'black';
+  e.target.style.backgroundColor = color;
+}
+
+// stop drawing when mouse is released
+function stopDrawing() {
+  isDrawing = false;
+}
+
+function clearGrid() {
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((pixel) => (pixel.style.backgroundColor = ''));
+}
+
+// add global event listener
+document
+  .querySelector('.grid-container')
+  .addEventListener('mouseleave', stopDrawing); // stop drawing when mouse leave the the grid
+document.addEventListener('mouseup', stopDrawing); // stop drawing when mouse is released anywhere
