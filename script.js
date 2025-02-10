1284dc2347d18dd9885ba0283e8addb8f7fecb82
Kamal Redzwan <109417261+kamal-redzwan@users.noreply.github.com>
2025-02-10 12:45:26 +0800

    add grid creation functionality and styling for Etch-A-Sketch

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello from JS');
+function createGrid() {
+  const gridContainer = document.querySelector('.grid-container');
+  const gridSize = 16;
+
+  const containerSize = 640;
+  const pixelSize = containerSize / gridSize;
+
+  const totalPixels = gridSize * gridSize;
+
+  for (let i = 0; i < totalPixels; i++) {
+    // create pixel div
+    const pixel = document.createElement('div');
+
+    // add pixel class
+    pixel.className = 'pixel';
+    // add pixel sizing
+    pixel.style.width = `${pixelSize}px`;
+    pixel.style.height = `${pixelSize}px`;
+
+    // append to the container
+    gridContainer.appendChild(pixel);
+  }
+}
+
+createGrid();
